f9544d29c6847fef36a56f2930443bd70cd1e2d7
GregSandoval <gregorysandoval.a@gmail.com>
2016-03-19 12:50:44 -0700

    Updated files to have the proper settings. Should compile fine for anyone cloning the project.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,23 +18,22 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 
 
 public class Main extends Application {
 
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         /*
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
@@ -94,16 +93,8 @@ public class Main extends Application {
         grid.add(userTextField, 0, 2);
 
 
-
-
         primaryStage.show();
 
 
-
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
